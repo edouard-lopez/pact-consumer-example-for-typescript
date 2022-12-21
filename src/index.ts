2b74abcc7be4fd1ab636a1d61a9466dd82c53d70
Édouard Lopez <contact@edouard-lopez.com>
2022-12-21 18:16:39 +0100

    refactor: add types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-export const api = (baseUrl = 'http://localhost'): any => ({
-  getCustomers: (): any =>
+export type Customer = { firstname: string; lastname: string };
+export type Customers = Array<Customer>;
+type Apis = {
+  getCustomers: () => Promise<Customers>;
+};
+
+export const api = (baseUrl = 'http://localhost'): Apis => ({
+  getCustomers: (): Promise<Customers> =>
     axios.get(`${baseUrl}/api/customers`).then((response) => response.data),
 });

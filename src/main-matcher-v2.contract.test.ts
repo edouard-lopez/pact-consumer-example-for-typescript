ea21fccc89d3fba23b09a0f456aa5f0de9f0c671
Édouard Lopez <contact@edouard-lopez.com>
2022-12-21 16:10:38 +0100

    test(pact): add pact-js@v3 example WIP
    fix the import: 'jest-pact/dist/v3'

--- src/main-matcher-v2.contract.test.ts
+++ src/main-matcher-v2.contract.test.ts
@@ -2,38 +2,41 @@ import { pactWith } from 'jest-pact';
 import { Matchers } from '@pact-foundation/pact';
 import { api } from './main.js';
 
-pactWith({ consumer: 'ms.cart', provider: 'ms.customers' }, (provider) => {
-  let client;
-  const CUSTOMERS_COUNT = 3;
-  beforeEach(() => {
-    client = api(provider.mockService.baseUrl);
-  });
-
-  beforeEach(() =>
-    provider.addInteraction({
-      state: 'Customers are available',
-      uponReceiving: 'A request for API customers',
-      willRespondWith: {
-        status: 200,
-        body: Matchers.eachLike(
-          { firstname: 'foo', lastname: 'bar' },
-          { min: CUSTOMERS_COUNT },
-        ),
-      },
-      withRequest: {
-        method: 'GET',
-        path: '/api/customers',
-      },
-    }),
-  );
-
-  describe('customers endpoint', () => {
-    it('returns customers', async () => {
+pactWith(
+  { consumer: 'ms.cart:with-pact@v2', provider: 'ms.customers' },
+  (provider) => {
+    let client;
+    const CUSTOMERS_COUNT = 3;
+    beforeEach(() => {
       client = api(provider.mockService.baseUrl);
+    });
 
-      await client.getCustomers().then((customers) => {
-        expect(customers.length).toEqual(CUSTOMERS_COUNT);
+    beforeEach(() =>
+      provider.addInteraction({
+        state: 'Customers are available',
+        uponReceiving: 'A request for API customers',
+        willRespondWith: {
+          status: 200,
+          body: Matchers.eachLike(
+            { firstname: 'foo', lastname: 'bar' },
+            { min: CUSTOMERS_COUNT },
+          ),
+        },
+        withRequest: {
+          method: 'GET',
+          path: '/api/customers',
+        },
+      }),
+    );
+
+    describe('customers endpoint', () => {
+      it('returns customers', async () => {
+        client = api(provider.mockService.baseUrl);
+
+        await client.getCustomers().then((customers) => {
+          expect(customers.length).toEqual(CUSTOMERS_COUNT);
+        });
       });
     });
-  });
-});
+  },
+);
---
+++ src/main-matcher-v3.contract.test.ts
@@ -0,0 +1,39 @@
+import { pactWith } from 'jest-pact/dist/v3';
+import { MatchersV3 as Matchers } from '@pact-foundation/pact';
+import { api } from './main.js';
+
+const CUSTOMERS_COUNT = 3;
+
+pactWith(
+  { consumer: 'ms.cart:with-pact@v3', provider: 'ms.customers' },
+  (interaction) => {
+    interaction('customers endpoint', ({ provider, execute }) => {
+      beforeEach(() =>
+        provider
+          .given('Customers are available')
+          .uponReceiving('A request for API customers')
+          .withRequest({
+            method: 'GET',
+            path: '/api/customers',
+          })
+          .willRespondWith({
+            status: 200,
+            body: Matchers.constrainedArrayLike(
+              { firstname: 'foo', lastname: 'bar' },
+              CUSTOMERS_COUNT,
+              CUSTOMERS_COUNT,
+            ),
+          }),
+      );
+
+      execute('some api call', (mockserver) =>
+        api(mockserver.url)
+          .getCustomers()
+          .then((customers) => {
+            // eslint-disable-next-line jest/no-standalone-expect
+            expect(customers.length).toEqual(CUSTOMERS_COUNT);
+          }),
+      );
+    });
+  },
+);

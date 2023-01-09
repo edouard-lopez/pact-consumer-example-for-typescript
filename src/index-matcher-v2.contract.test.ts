f6bec5c53276c0693f55a00c394ac7d10a253300
Édouard Lopez <edouard.lopez@manomano.com>
2023-01-09 12:20:16 +0100

    feat: add /api/customer/:customerId/orders endpoint and test it

--- src/index-matcher-v2.contract.test.ts
+++ src/index-matcher-v2.contract.test.ts
@@ -6,13 +6,14 @@ pactWith(
   { consumer: 'ms.cart:with-pact@v2', provider: 'ms.customers' },
   (provider) => {
     let client;
-    const CUSTOMERS_COUNT = 3;
     beforeEach(() => {
       client = api(provider.mockService.baseUrl);
     });
 
     describe('customers endpoint', () => {
       it('returns customers', async () => {
+        const CUSTOMERS_COUNT = 3;
+
         provider.addInteraction({
           state: 'Customers are available',
           uponReceiving: 'A request for API customers',
@@ -27,13 +28,49 @@ pactWith(
             method: 'GET',
             path: '/api/customers',
           },
-        })
+        });
 
         client = api(provider.mockService.baseUrl);
 
         await client.getCustomers().then((customers) => {
           expect(customers.length).toEqual(CUSTOMERS_COUNT);
         });
+      });
+
+      it("returns customer' orders", async () => {
+        const CUSTOMER_ORDERS_COUNT = 3;
+        provider.addInteraction({
+          state: 'Customers orders are available',
+          uponReceiving: 'A request for API customer-orders',
+          willRespondWith: {
+            status: 200,
+            body: {
+              customer: Matchers.uuid('2099d96b-1693-4f6f-b218-130238346855'),
+              orders: Matchers.eachLike(
+                {
+                  id: Matchers.string('bddb4566-b025-4f1c-9784-802402e5ea9b'),
+                  quantity: Matchers.integer(2),
+                },
+                { min: CUSTOMER_ORDERS_COUNT },
+              ),
+            },
+          },
+          withRequest: {
+            method: 'GET',
+            path: '/api/customer/2099d96b-1693-4f6f-b218-130238346855/orders',
+          },
+        });
+
+        client = api(provider.mockService.baseUrl);
+
+        await client
+          .getCustomerOrders('2099d96b-1693-4f6f-b218-130238346855')
+          .then((customer_orders) => {
+            expect(customer_orders.customer).toBe(
+              '2099d96b-1693-4f6f-b218-130238346855',
+            );
+            expect(customer_orders.orders.length).toBe(CUSTOMER_ORDERS_COUNT);
+          });
       });
     });
   },
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,20 @@ import axios from 'axios';
 export type uuid = string;
 export type Customer = { id: uuid; firstname: string; lastname: string };
 export type Customers = Array<Customer>;
+export type CustomerOrder = { id: uuid; quantity: number };
+export type CustomerOrders = { customer: uuid; orders: Array<CustomerOrder> };
+
 type Apis = {
   getCustomers: () => Promise<Customers>;
+  getCustomerOrders: (customer: uuid) => Promise<CustomerOrders>;
 };
 
 export const api = (baseUrl = 'http://localhost'): Apis => ({
   getCustomers: (): Promise<Customers> =>
     axios.get(`${baseUrl}/api/customers`).then((response) => response.data),
+
+  getCustomerOrders: (customer: uuid): Promise<CustomerOrders> =>
+    axios
+      .get(`${baseUrl}/api/customer/${customer}/orders`)
+      .then((response) => response.data),
 });
